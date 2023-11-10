83ff0294aad758cd8d84ff66972aa6a710bc32ed
Yiheng Gao <gao43@uwindsor.ca>
2023-11-09 22:20:11 -0500

    asdfasf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignInPage from './SignIn'; // Ensure this is the correct path
+import SignInPage from './SignIn'; // Ensure this is the correct pathwrwrw
 import MainPage from './MainPage'; // Ensure this is the correct path
 
 function App() {
